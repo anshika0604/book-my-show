29086066d0fcc0259145f90b6cf4ded91145e290
Anshika <anshikanegi.cs19@gmail.com>
2023-08-29 20:26:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import "./App.css"
+import {Routes, Route} from 'react-router-dom'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MoviePage />}/>
+      <Route path="/plays" element={<Plays />}/>
+    </Routes>
   );
 }
 
