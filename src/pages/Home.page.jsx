d4e57cbaf6722e8cc7acc2210707c08a739c8c23
Anshika <anshikanegi.cs19@gmail.com>
2023-08-31 21:45:54 +0530

    updated

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -31,7 +31,7 @@ const HomePage = () => {
 
         <div className='bg-premier-800 py-12'>
         <div className='container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3'>
-            <div className='md:flex'>
+            <div className='hidden md:flex'>
             <img src='https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png' alt="Rupay" className='w-full h-full' />
 
             </div>
