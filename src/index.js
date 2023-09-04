fae9a7fe0a42b4816845a84cdbee06418e57afb1
Anshika <anshikanegi.cs19@gmail.com>
2023-09-04 13:04:39 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import MovieProvider from './context/Movie.context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <MovieProvider >
       <App />
     </MovieProvider>
